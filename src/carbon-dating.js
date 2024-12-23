dcbb9ac146f7a4c80da02a4b3f3bfd9679cf972a
Elidion-netizen <sapegaserg@gmail.com>
2024-12-23 20:04:59 +0300

    feat: add carbon-dating task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -18,19 +18,13 @@ const HALF_LIFE_PERIOD = 5730;
  *
  */
 function dateSample( sampleActivity ) {
-  if (typeof sampleActivity !== 'string' || isNaN(parseFloat(sampleActivity))) {
-    return false;
-  }
-
   const activity = parseFloat(sampleActivity);
 
-  if (activity <= 0 || activity > MODERN_ACTIVITY) {
+  if (typeof sampleActivity !== 'string' || isNaN(activity) || activity <= 0 || activity > MODERN_ACTIVITY) {
     return false;
   }
 
-  const age = Math.ceil(Math.log(MODERN_ACTIVITY / activity) / (0.693 / HALF_LIFE_PERIOD));
-
-  return age;
+  return Math.ceil(Math.log(MODERN_ACTIVITY / activity) / (0.693 / HALF_LIFE_PERIOD))
 }
 
 module.exports = {
