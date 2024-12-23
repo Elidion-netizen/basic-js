eca9daa3088fa24dfa4137b719aae1936dcc2f3e
Elidion-netizen <sapegaserg@gmail.com>
2024-12-23 21:03:08 +0300

    feat: add transform array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,23 +1,53 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create transformed array based on the control sequences that original
  * array contains
- * 
+ *
  * @param {Array} arr initial array
  * @returns {Array} transformed array
- * 
+ *
  * @example
- * 
+ *
  * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
- * 
+ *
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+  if (!arr.length) {
+    return arr;
+  }
+
+  const resultArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    const lastElement = resultArr[resultArr.length - 1];
+    switch (arr[i]) {
+      case "--discard-next":
+        i++;
+        break;
+      case "--discard-prev":
+        typeof lastElement !== "undefined" &&
+          arr[i - 2] !== "--discard-next" &&
+          resultArr.splice(resultArr.length - 1, 1);
+        break;
+      case "--double-next":
+        typeof arr[i + 1] !== "undefined" && resultArr.push(arr[i + 1]);
+        break;
+      case "--double-prev":
+        typeof arr[i - 1] !== "undefined" &&
+          arr[i - 2] !== "--discard-next" &&
+          resultArr.push(arr[i - 1]);
+        break;
+      default:
+        resultArr.push(arr[i]);
+    }
+  }
+  return resultArr;
 }
 
 module.exports = {
-  transform
+  transform,
 };
